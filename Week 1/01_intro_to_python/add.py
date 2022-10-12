e731e6240c4b2147671e53a9f29980e89c450ce5
pinky-banik <pinky.rani.banik@gmail.com>
2022-10-12 13:43:01 +0600

    add nmbr input

---
+++ Week 1/01_intro_to_python/add.py
@@ -0,0 +1,7 @@
+num1 = input ('Tell us the first number: ')
+num2 = input ('Tell us the second number: ')
+num1_int = int(num1)
+#result = num1 + num2
+
+result = int(num1) + int(num2)
+print(result)
--- Week 1/01_intro_to_python/input.py
+++ Week 1/01_intro_to_python/input.py
@@ -1,2 +1,7 @@
-input()
+# input()
+age = input ('Tell us about your age: ')
 print('I can take input now.')
+print(age)
+print(type(age))
+
+
