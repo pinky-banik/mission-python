65797349ce332a45fed7ea21a590b308c110e563
pinky-banik <pinky.rani.banik@gmail.com>
2022-10-14 15:22:08 +0600

    tuple

---
+++ Week 2/05_list_set_dictionary_and_tuples/tuple.py
@@ -0,0 +1,15 @@
+# numbers = [12,45,56,45,12,89]
+# print(numbers)
+# numbers_tuple = 12,45,56,45,12,89
+# print(numbers_tuple)
+
+# # tuple is immutable. means you can not change this
+# nums_tuple = (12,45,56)
+# print(nums_tuple[0])
+# # nums_tuple[0] = 12
+# print(nums_tuple[0])
+
+# but you can change insie tuple if the element is changeble
+tuple2D = ([12,45,12],[45,11])
+tuple2D[0][1] = 99
+print(tuple2D)
