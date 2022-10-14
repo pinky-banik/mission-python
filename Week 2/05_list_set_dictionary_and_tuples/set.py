898e9e0f7e7974a14320f19cdc2d9bf7649d8f53
pinky-banik <pinky.rani.banik@gmail.com>
2022-10-14 15:04:06 +0600

    set

--- Week 2/05_list_set_dictionary_and_tuples/set.py
+++ Week 2/05_list_set_dictionary_and_tuples/set.py
@@ -2,3 +2,10 @@ numbers = [12, 45,56, 45, 12]
 print(numbers)
 nums = {12,45,56,45,12,89}
 print(nums)
+
+numbers_set = set(numbers)
+print(numbers_set)
+numbers_set.add(77) # only addes whom is not already exists
+numbers_set.add(45) # if the element is already exists then it will not add
+print(numbers_set)
+print (len(numbers_set))
