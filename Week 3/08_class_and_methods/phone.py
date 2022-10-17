b8fd2c51dfa22b881a3e7c2250ab6b92e8257a2a
pinky-banik <pinky.rani.banik@gmail.com>
2022-10-17 15:33:01 +0600

    class

---
+++ Week 3/08_class_and_methods/phone.py
@@ -0,0 +1,18 @@
+class phone:
+  brand = 'samsung'
+  color = 'black'
+  price = 25000
+
+my_phone = phone()
+print(my_phone)
+print(my_phone.brand)
+print(my_phone.price)
+
+
+your_phone = phone()
+
+her_phone = phone()
+
+dad_phone = phone()
+
+print(dad_phone.brand)
